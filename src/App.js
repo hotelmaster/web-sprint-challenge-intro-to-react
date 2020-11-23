a78c524272b8784e2fe9b9de4af7f0639013375c
hotelmaster <hotelmasternm@gmail.com>
2020-11-22 20:55:52 -0800

    import state hook and use it to initialize a slice of state for the character info found in the Rick and Morty API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+
+  // use state hook and destructuring to set up the slice of state
+  const [characters, setCharacters] = useState(charArr);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
